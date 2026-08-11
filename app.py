df63d609a4b7cfab837d7d055516913120d239b5
mochachecreflo-prog <mochachecreflo@gmail.com>
2026-08-11 16:28:05 +0300

    Remove duplicate db.create_all() from load_user

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize Extensions
 db = SQLAlchemy(app)
-with app.app_context():
-    db. create_all()
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
